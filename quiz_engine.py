9fb23ff7c44ab9b7e93d79a3e558a5f908aac610
Monnorthegodlydev <141607683+Monnorthegodlydev@users.noreply.github.com>
2023-09-14 16:33:02 +0800

    Code

--- quiz_engine.py
+++ quiz_engine.py
@@ -1,19 +1,15 @@
 import os
-try:
-    import time
-    from fuzzywuzzy import fuzz
-    import sys
-    import clearterm
-except:
-    os.system('pip install fuzzywuzzy > nul')
-clearterm.clear_terminal()
-os.system('pip install python-Levenshtein > nul')
-clearterm.clear_terminal()
+import time
+from fuzzywuzzy import fuzz
+import sys
+import clearterm
+
 local_qnum = 1
 qadata = {}
+score = 0
 
 print(f'Arguments provided: {sys.argv[1:]}')
-print('Prosessing...')
+print('Processing...')
 time.sleep(3)
 clearterm.clear_terminal()
 
@@ -56,17 +52,16 @@ try:
         debugforlencheck1 = len(sys.argv) < 2
         print(f"length of sys.argv is {len(sys.argv)}. If it is higher than 2, it should work, but if the number is odd, it won't work, and here is the bool for len(sys.argv) < 2: {debugforlencheck1}")
         print(f"Also, this is the bool for len(sys.argv) % 2 != 0: {debugforlencheck2}")
-        if ValueError :
+        if ValueError:
             print("You did not provide a sufficient number of arguments. It should be structured as 'q1 a1 q2 a2, etc.'")
             input('Press Enter to Continue...')
             exit()
-    
 
     for i in range(1, len(sys.argv), 2):
         que_key = 'q' + str(local_qnum)
         ans_key = 'a' + str(local_qnum)
-        qadata[que_key] = sys.argv[i].replace("'", " ")
-        qadata[ans_key] = sys.argv[i + 1].replace("'", " ")
+        qadata[que_key] = sys.argv[i].replace("'", " ").replace('"', ' ')
+        qadata[ans_key] = sys.argv[i + 1].replace("'", " ").replace('"', ' ')
         local_qnum += 1
 except ValueError as e:
     print(e)
@@ -77,11 +72,14 @@ except IndexError as ie:
 def quiz_ai_processor(A, Qnum, que_key, threshold=80):
     ans_key = 'a' + str(Qnum)
     similarity_score = fuzz.token_sort_ratio(A.lower(), qadata[ans_key].lower())
+    global robotans
     robotans = similarity_score >= threshold
     if robotans:
-        return('Correct!!! Good Job :)')
+        global score
+        score += 1
+        print('Correct!!! Good Job :)')
     else:
-        return(f'That is Not Correct.')
+        print(f'That is Not Correct.')
 
 local_qnum = 1
 try:
@@ -89,9 +87,9 @@ try:
     while local_qnum <= len(sys.argv[1:]) // 2:
         que_key = 'q' + str(local_qnum)
         usea = input(qadata.get(que_key, f'No question available from q{que_key}') + ': ')
-        print(quiz_ai_processor(usea, local_qnum, que_key, 80))
+        quiz_ai_processor(usea, local_qnum, que_key, 80)
         local_qnum += 1
-    print('The Quiz is Over, Have a Nice day!!')
+    print(f'The Quiz is Over, Have a Nice day!! \n Your score is {score}')
     input('Press Enter to Exit...')
     exit()
 
@@ -99,4 +97,3 @@ except KeyboardInterrupt:
     print('\nQuiz Stopped.')
 except Exception as e:
     print(f'Oops, there was an error: {str(e)}')
-    
